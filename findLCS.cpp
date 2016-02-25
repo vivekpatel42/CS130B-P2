7f11b71a1e3830858fe8201d73ee8749d48b01b5
Vivek Patel <vivekp42095@gmail.com>
2016-02-24 21:03:10 -0800

    Completed parts 1 and 2 of the project (LCS length and print LCS).

--- findLCS.cpp
+++ findLCS.cpp
@@ -6,23 +6,23 @@
 
 using namespace std;
 
-typedef struct { 
+typedef struct {
 	int** length;
 	char** direction;
 	int lcsLength;
 } LCS, *LCS_PTR;
 
 LCS_PTR findLCSLength(char* m, char* n, int x, int y);
-void printLCS(char** dMatrix, char* str, int i, int j);
+void printLCS(char** dMatrix, char* str, int i, int j, int index);
 
-int main() 
+int main()
 {
 	int n, k, x, y, i = 0;
 	string line;
 	char *token, *tempStr;
 	char* strArr[2], *str1, *str2;
 	LCS_PTR lcsResult = new LCS;
-	while (getline(cin,line))
+	while (getline(cin, line))
 	{
 		k = 0;
 		if (i == 0) {
@@ -33,7 +33,7 @@ int main()
 			tempStr = new char[line.size() + 1];
 			strcpy(tempStr, line.c_str());
 			token = strtok(tempStr, " ");
-			while(token != NULL) {
+			while (token != NULL) {
 				strArr[k++] = token;
 				token = strtok(NULL, " ");
 			}
@@ -43,12 +43,12 @@ int main()
 			printf("%d, %d\n", x, y);
 			lcsResult = findLCSLength(str1, str2, x, y);
 			printf("The longest common subsequence between %s and %s is of size %d.\n", str1, str2, lcsResult->lcsLength);
-			printLCS(lcsResult -> direction, str1, x-1, y-1);
+			printLCS(lcsResult -> direction, str1, x, y, (lcsResult -> length)[x][y]);
 		}
 	}
-	
+
 	return 1;
-	for(int i = 0; i < x; i++) {
+	for (int i = 0; i < x; i++) {
 		delete[] (lcsResult -> length)[i];
 		delete[] (lcsResult -> direction)[i];
 	}
@@ -60,37 +60,37 @@ int main()
 LCS_PTR findLCSLength(char* m, char* n, int x, int y) {
 	LCS_PTR result = new LCS;
 	int max = 0;
-	int** lengthMatrix = new int*[x];
-	char** directionalMatrix = new char*[x];
-	for(int i = 0; i < x; i++) {
-		lengthMatrix[i] = new int[y];
-		directionalMatrix[i] = new char[y];
+	int** lengthMatrix = new int*[x + 1];
+	char** directionalMatrix = new char*[x + 1];
+	for (int i = 0; i <= x; i++) {
+		lengthMatrix[i] = new int[y + 1];
+		directionalMatrix[i] = new char[y + 1];
 	}
-	for(int i = 1; i < x; i++) {
+	for (int i = 1; i <= x; i++) {
 		lengthMatrix[i][0] = 0;
 	}
-	for(int j = 1; j < y; j++) {
+	for (int j = 1; j <= y; j++) {
 		lengthMatrix[0][j] = 0;
 	}
-	for(int i = 1; i < x; i++) {
-		for(int j = 1; j < y; j++) {
-			if(m[i] == n[j]) {
-				lengthMatrix[i][j] = lengthMatrix[i-1][j-1] + 1;
+	for (int i = 1; i <= x; i++) {
+		for (int j = 1; j <= y; j++) {
+			if (m[i - 1] == n[j - 1]) {
+				lengthMatrix[i][j] = lengthMatrix[i - 1][j - 1] + 1;
 				directionalMatrix[i][j] = 'D';
 			}
-			else if(lengthMatrix[i-1][j] >= lengthMatrix[i][j-1]) {
-				lengthMatrix[i][j] = lengthMatrix[i][j-1];
+			else if (lengthMatrix[i - 1][j] > lengthMatrix[i][j - 1]) {
+				lengthMatrix[i][j] = lengthMatrix[i - 1][j];
 				directionalMatrix[i][j] = 'U';
 			}
 			else {
-				lengthMatrix[i][j] = lengthMatrix[i][j-1];
+				lengthMatrix[i][j] = lengthMatrix[i][j - 1];
 				directionalMatrix[i][j] = 'L';
 			}
 		}
 	}
-	for(int i = 0; i < x; i++) {
-		for(int j = 0; j < y; j++) {
-			if(lengthMatrix[i][j] >= max) 
+	for (int i = 0; i <= x; i++) {
+		for (int j = 0; j <= y; j++) {
+			if (lengthMatrix[i][j] > max)
 				max = lengthMatrix[i][j];
 		}
 	}
@@ -100,19 +100,20 @@ LCS_PTR findLCSLength(char* m, char* n, int x, int y) {
 	return result;
 }
 
-void printLCS(char** dMatrix, char* str, int i, int j) {
-	if(i == 0 || j == 0) {
-		printf("\n");
-		return;
+void printLCS(char** dMatrix, char* str, int i, int j, int index) {
+	char lcs[index + 1];
+	lcs[index] = '\0';
+	while (i > 0 && j > 0)
+	{
+		if (dMatrix[i][j] == 'D')
+		{
+			lcs[index - 1] = str[i - 1]; // Put current character in result
+			i--; j--; index--;     // reduce values of i, j and index
+		}
+		else if (dMatrix[i][j] == 'U')
+			i--;
+		else
+			j--;
 	}
-	if(dMatrix[i][j] == 'D') {
-		printLCS(dMatrix, str, i-1, j-1);
-		printf("%c", str[i]);
-	}
-	else if(dMatrix[i][j] == 'U') {
-		printLCS(dMatrix, str, i-1, j);
-	}
-	else {
-		printLCS(dMatrix, str, i, j-1);
-	}
+	printf("%s\n", lcs);
 }
